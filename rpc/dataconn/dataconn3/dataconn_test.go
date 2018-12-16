74cd381fe53b63f8c0e0f7180952cc89d77a105b
Christian Schwarz <me@cschwarz.com>
2018-12-16 23:27:52 +0100

    more deletions

--- rpc/dataconn/dataconn3/dataconn_test.go
+++ rpc/dataconn/dataconn3/dataconn_test.go
@@ -1,14 +1,1 @@
 package dataconn
-
-import (
-	"bytes"
-	"sync"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"github.com/zrepl/zrepl/logger"
-	"github.com/zrepl/zrepl/rpc/dataconn/frameconn2"
-	"github.com/zrepl/zrepl/util/socketpair"
-)
-
